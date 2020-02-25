4f1d654d8d86c0dc0de4ae7b45550714ccca17d2
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-02-25 04:35:47 +0200

    vv

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,7 @@
 jQuery(function ($) {
 
-    $(".notification a").click(function () {
-        $(".FAQDropdown").css("display","none");
-        $(".profileDropDown").css("display","none");
-
-        $(".notifyDropDown").toggle();
-    });
-    $(".faqToggle a").click(function () {
-        $(".faqToggle p").css("display", "none");
-        $(".faqToggle i").removeClass("fa-chevron-up");
-        $(".faqToggle i").addClass("fa-chevron-down");
-        $(this).parent().find("p").toggle();
-        if ($(this).parent().find("i").hasClass("fa-chevron-down")) {
-            $(this).parent().find("i").removeClass("fa-chevron-down");
-            $(this).parent().find("i").addClass("fa-chevron-up");
-
-        }
-        else{
-            $(".faqToggle i").addClass("fa-chevron-down");
-        }
-    });
-    $(".fAQ a").click(function () {
-      $(".notifyDropDown").css("display","none");
-      $(".profileDropDown").css("display","none");
-        $(".FAQDropdown").toggle();
-    });
-    $(".personalInfo").click(function () {
-        $(".notifyDropDown").css("display","none");
-        $(".FAQDropdown").css("display","none");
-          $(".profileDropDown").toggle();
-      });
-      $(".bookDetailsBtn").click(function(){
-        $(".StatisticsBox").css("display","block");
-      })
-    
+$(".settingsSec").click(function(){
+    $(this).find(".settingBox").css("display","block");
+});
 });
 
