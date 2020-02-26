5c9c58b1b9d72c20f6f7bd9260f8aeac90b86296
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-02-26 23:11:32 +0200

    ddd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,26 @@
 jQuery(function ($) {
 
-$(".settingsSec").click(function(){
-    $(this).find(".settingBox").css("display","block");
-});
+    $(".settingsSec").click(function () {
+        $(this).find(".settingBox").css("display", "block");
+        $(".whiteOverlay").css("display", "block");
+        $(".bookImg").addClass("blure");
+
+    });
+    $('body').click(function (evt) {
+  
+           
+        //For descendants of menu_content being clicked, remove this check if you do not want to put constraint on descendants.
+        if ($(evt.target).closest('.settingsSec').length>0){
+            return false;
+        }
+       
+        else{
+            $(this).find(".settingBox").css("display", "none");
+            $(".whiteOverlay").css("display", "none");
+        }
+        //Do processing of click event here for every element except with id menu_content
+
+    });
+
 });
 
